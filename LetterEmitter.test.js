967aba5cc43d65b8388a9268f64c7aa139200dfc
Zach Butler <zach.standley93@gmail.com>
2019-07-09 15:46:24 -0700

    started letteremitter test, made new letteremitter before each test

---
+++ LetterEmitter.test.js
@@ -0,0 +1,11 @@
+const LetterEmitter = require('./LetterEmitter');
+
+describe('LetterEmitter', () => {
+    beforeEach(() => {
+        let LetterEmitter = new LetterEmitter();
+    });
+
+    it('splits a string and emits an event for each letter', done => {
+        
+    });
+});
