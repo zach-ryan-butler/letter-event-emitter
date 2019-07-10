9fa82ef4b9be911a7c73ee465b25d0629d4c42e9
Zach Butler <zach.standley93@gmail.com>
2019-07-09 17:05:05 -0700

    wrote stuff

--- LetterEmitter.js
+++ LetterEmitter.js
@@ -1,7 +1,16 @@
 const EventEmitter = require('events');
 
 class LetterEmitter extends EventEmitter {
-
+    read(str) {
+        const lettersArray = str.split('');
+        
+        for( let i = 0; i < lettersArray.length; i++) {
+            const letter = lettersArray[i]
+            this.emit('Letter', { letter, offset: i })
+        }
+        this.emit('end');
+    }
+      
 };
 
 module.exports = LetterEmitter;
--- LetterEmitter.test.js
+++ LetterEmitter.test.js
@@ -1,11 +1,26 @@
 const LetterEmitter = require('./LetterEmitter');
 
 describe('LetterEmitter', () => {
+    let letterEmitter;
     beforeEach(() => {
-        let LetterEmitter = new LetterEmitter();
+       letterEmitter = new LetterEmitter();
     });
 
     it('splits a string and emits an event for each letter', done => {
-        
+        const callback = jest.fn();
+        letterEmitter.on('Letter', data => {
+            callback(data)
+        });
+
+        letterEmitter.once('end', () => {
+            'hello world'.split('').forEach((letter, offset) => {
+                expect(callback).toHaveBeenCalledWith({
+                    letter,
+                    offset
+                });
+            });
+            done()
+        });
+        letterEmitter.read('hello world');
     });
 });
